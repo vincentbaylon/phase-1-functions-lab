28a1425d0cc8a370875df1ff3fce67b1eb521a32
vincentbaylon <vincentbaylon@gmail.com>
2021-07-05 20:59:36 -0700

    commit tree f3862dbea4c8a9d3034405fdd28160539180ac7a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return Math.abs((someValue - 42) * 264)
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    return Math.abs((num1 - num2) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    const doMath = (Math.abs(start - destination) * 264)
+
+    if (doMath > 2500) {
+        return 'cannot travel that far'
+    } else if (doMath > 2000) {
+        return 25
+    } else if ((doMath > 400) && (doMath < 2000)) {
+        return (doMath - 400) * .02
+    } else if (doMath <= 400) {
+        return 0
+    }
+}
